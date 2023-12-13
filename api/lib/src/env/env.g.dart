bda5acbe478c1afd1190664d428ac9ed96c70a78
Antony Lucas <54651622+Antony-Lucas@users.noreply.github.com>
2023-12-12 22:07:57 -0300

    Added project to supabase

---
+++ api/lib/src/env/env.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _Env {
+  static const String SUPABASE_URL = 'https://xusamvpprbrusontoliv.supabase.co';
+
+  static const String SUPABASE_ANON_URL =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1c2FtdnBwcmJydXNvbnRvbGl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIxNTY5MDIsImV4cCI6MjAxNzczMjkwMn0.S6J8yeXGEf78DlYjuOK0Y1nrEoNoshBJAmrZf6v3q4Q';
+
+  static const String SUPABASE_SERVICE_ROLE_KEY =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1c2FtdnBwcmJydXNvbnRvbGl2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjE1NjkwMiwiZXhwIjoyMDE3NzMyOTAyfQ.E2ETgYFfC5Q5kBy3wvFzboDc6w6X2INV0iwYrt02bAE';
+}
